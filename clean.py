451a28f86e1c5fa37c5040a11e970912b8b23078
Githika <gannapur@ucsc.edu>
2025-05-05 18:50:11 -0700

    code works. tested it on 3 of my accounts

--- clean.py
+++ clean.py
@@ -6,6 +6,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from datetime import datetime, timedelta, timezone
 
 # If modifying these SCOPES, delete the token.json file.
 #SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
@@ -31,26 +32,51 @@ def authenticate_gmail():
 
 def get_unread_emails_older_than_1_month(service):
     """Returns list of message IDs that are unread and older than 1 month."""
-    # Get date 30 days ago
-    one_month_ago = (datetime.datetime.utcnow() - datetime.timedelta(days=30)).strftime("%Y/%m/%d")
+    # Get date 30 days ago (timezone aware)
+    one_month_ago = (datetime.now(timezone.utc) - timedelta(days=30)).strftime("%Y/%m/%d")
     query = f'is:unread before:{one_month_ago}'
-    
-    results = service.users().messages().list(userId='me', q=query).execute()
-    messages = results.get('messages', [])
-    
-    if not messages:
+
+    all_messages = []
+    next_page_token = None
+
+    while True:
+        response = service.users().messages().list(
+            userId='me',
+            q=query,
+            maxResults=500,  # max allowed by Gmail API
+            pageToken=next_page_token
+        ).execute()
+
+        messages = response.get('messages', [])
+        all_messages.extend([msg['id'] for msg in messages])
+
+        next_page_token = response.get('nextPageToken')
+        if not next_page_token:
+            break
+
+    if not all_messages:
         print('No unread emails older than 1 month.')
         return []
-    
-    print(f'Found {len(messages)} unread emails older than 1 month.')
-    return [msg['id'] for msg in messages]
+
+    print(f'Found {len(all_messages)} unread emails older than 1 month.')
+    return all_messages
 
 def delete_messages(service, message_ids):
-    """Batch deletes the messages with the given IDs."""
-    if not message_ids:
-        return
-    service.users().messages().batchDelete(userId='me', body={'ids': message_ids}).execute()
-    print(f'Deleted {len(message_ids)} emails.')
+    """Deletes messages in batches of 1000 (Gmail API limit)."""
+    BATCH_SIZE = 1000
+    total = len(message_ids)
+
+    print(f'Deleting {total} messages in batches of {BATCH_SIZE}...')
+
+    for i in range(0, total, BATCH_SIZE):
+        batch = message_ids[i:i + BATCH_SIZE]
+        service.users().messages().batchDelete(
+            userId='me',
+            body={'ids': batch}
+        ).execute()
+        print(f'Deleted batch {i//BATCH_SIZE + 1}: {len(batch)} messages')
+
+    print('All messages deleted.')
 
 def main():
     service = authenticate_gmail()
@@ -61,3 +87,5 @@ if __name__ == '__main__':
     main()
 
 
+
+# if you want to change the user, run 'rm token.json' you will be prompted to log in again
